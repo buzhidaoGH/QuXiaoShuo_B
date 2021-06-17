90df077922ce86a4cc45a06425758ef0057fc755
buzhidaoGH <2368244675@qq.com>
2021-06-17 14:38:21 +0800

    趣小说后台建设开始

---
+++ src/main/java/com/localhost/quxiaoshuo/QuxiaoshuoApplication.java
@@ -0,0 +1,13 @@
+package com.localhost.quxiaoshuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuxiaoshuoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuxiaoshuoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/localhost/quxiaoshuo/QuxiaoshuoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localhost.quxiaoshuo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuxiaoshuoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
