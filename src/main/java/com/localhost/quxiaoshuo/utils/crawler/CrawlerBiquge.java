82f858e283247ac7a367a0b3ec0fcfeeb8dd1903
buzhidaoGH <2368244675@qq.com>
2021-06-17 17:06:05 +0800

    测试连接v1.0.0

--- src/main/java/com/localhost/quxiaoshuo/utils/crawler/CrawlerBiquge.java
+++ src/main/java/com/localhost/quxiaoshuo/utils/crawler/CrawlerBiquge.java
@@ -34,6 +34,10 @@ public class CrawlerBiquge implements PageProcessor {
 	 * 可以返回默认配置
 	 */
 	private Site site = Site.me()
+			.setCharset("gbk")//编码格式
+			.setRetryTimes(3)//重试次数
+			.addHeader("Referer", "http://www.biquge.tv/")//设置跳转前页面
+			.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36")
 			.setTimeOut(3000);//超时时间3s
 
 	@Override
@@ -41,11 +45,14 @@ public class CrawlerBiquge implements PageProcessor {
 		return site;
 	}
 
+	private static Spider spider = Spider.create(new CrawlerBiquge())
+			//设置起始url
+			.addUrl("http://www.biquge.tv/0_63352/");
+
 	public static void main(String[] args) {
-		Spider.create(new CrawlerBiquge())
-				//设置起始url
-				.addUrl("http://www.biquge.tv/0_63352/")
-				//启动爬虫
-				.run();
+		spider.run();
+		//启动爬虫(同步方法,当前线程中执行,完成此次进行下一次);start同时进行(新建线程)
+		// .run();
+		// .start();
 	}
 }
