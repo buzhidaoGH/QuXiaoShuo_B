eb46e835e145fb4bf265e50037992e59c4147be5
buzhidaoGH <2368244675@qq.com>
2021-07-14 20:40:32 +0800

    v1.0.0某个bug修复

--- src/test/java/com/localhost/quxiaoshuo/QuxiaoshuoApplicationTests.java
+++ src/test/java/com/localhost/quxiaoshuo/QuxiaoshuoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.localhost.quxiaoshuo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class QuxiaoshuoApplicationTests {
 
+	@Autowired
+	private RedisTemplate redisTemplate;
+
 	@Test
 	void contextLoads() {
+		//redisTemplate的用法 opsForValue 操作字符串,类似String
+		//redisTemplate的用法 opsForList 操作List,类似List
+		
 	}
 
 }
